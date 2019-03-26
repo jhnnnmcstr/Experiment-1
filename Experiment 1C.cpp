2f2a13d0b6d73ea52e819774c1d4f03910827564
Johanna Naomi C. Castro <47728312+jhnnnmcstr@users.noreply.github.com>
2019-03-26 20:55:52 +0800

    Update Experiment 1C.cpp

--- Experiment 1C.cpp
+++ Experiment 1C.cpp
@@ -1,16 +1,17 @@
-// Interest on a credit’s card unpaid balance is calculated using the average daily. 
+// Interest on a creditâ€™s card unpaid balance is calculated using the average daily. 
 // Suppose that netBalance is the balance shown in the bill, 
 // payment is the payment made, d1 is the number of days in the billing cycle, 
 // and d2 is the number of days payment is made before billing cycle. 
 // Then the average daily
 // balance is:
-// averageDailyBalance = (netBalance * d1 – payment * d2) / d1
+// averageDailyBalance = (netBalance * d1 â€“ payment * d2) / d1
 // If the interest rate per month is say, 0.0152, then the interest on the unpaid balance is:
 // interest = (averageDailyBalance * 0.0152)
 // Write a program that accepts as input netBalance, payment, d1, d2, and interest rate per month. 
 // The program outputs the interest. Format your output to two decimal places.	
 
 #include <iostream>
+#include <conio.h>
 #include <iomanip>
 
 using namespace std;
@@ -37,6 +38,7 @@ int main()
 	interest = (averageDailyBalance * monthlyInterestRate);
 	
 	cout << "Interest is: " << interest << endl;
-	return 0;
 
+	-getch();
+	return 0;
 }
